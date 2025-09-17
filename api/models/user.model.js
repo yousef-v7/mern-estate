571d0d96272a739ab3dc3011418867bda848a9ce
yousef <yousef.ahmed7@gmail.com>
2025-09-18 01:57:41 +0300

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
+
