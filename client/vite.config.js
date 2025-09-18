d0dec327531ea071ced399d8f30335cc5e5c8904
yousef <yousef.ahmed7@gmail.com>
2025-09-19 02:22:08 +0300

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  // this for connect to backend server
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
+  plugins: [react(), tailwindcss()],
+});
